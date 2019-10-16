4be9853fe91c20d75599112eeb12719ba85796cc
Adam Goldfine <tulaneadam21@gmail.com>
2019-10-15 21:40:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var bowser =require('bowser');
+import Bowser from "bowser";
 var shell = require('shelljs');
 
-const platform = bowser.getOSName();
+componentDidMount() {
+    const bowser = Bowser.getParser(window.navigator.userAgent);
+    const platform = bowser.getOSName();
+}
 
 if (platform==='Linux'||'Chrome OS') {
 shell.exec(`sudo apt-get update && sudo apt-get upgrade && sudo apt-get install gnome-software gnome-packagekit && sudo apt-get update && sudo apt-get dist-upgrade && curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - && sudo apt-get install -y nodejs && wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - && echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list && sudo apt-get update && sudo apt-get install -y mongodb-org && sudo apt install nautilus && curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg && sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/ && sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' && sudo apt-get install apt-transport-https && sudo apt-get update && sudo apt-get install code && git config --global credential.helper cache`);
