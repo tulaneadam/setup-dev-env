7d4b32e274a70a41cf039e844b9a1290ff0e3b4b
Adam Goldfine <tulaneadam21@gmail.com>
2019-10-18 15:37:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -54,21 +54,19 @@ shell.echo(`choco install nodejs; choco install mongodb; choco install git.insta
 else if (platform==='linux' && distro==='Debian') {
 shell.exec(`sudo apt-get update`);
 shell.exec(`sudo apt-get install -y gnome-software gnome-packagekit`);
- shell.exec(`sudo apt install -y nautilus`) 
- shell.exec(`sudo apt-get install -y apt-transport-https`)
+ shell.exec(`sudo apt install -y nautilus`) ;
  shell.exec(`git config --global credential.helper cache`);
  //shell.exec(`sudo apt install -y snapd`);
  //shell.exec(`sudo snap install snap-store`);
  //shell.exec(`sudo snap install mongo32`);
  //shell.exec(`sudo snap install gitkraken`);
  //shell.exec(`sudo snap install code --classic`);
- shell.exec(`curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - && sudo apt-get install -y nodejs && wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -`);
+ shell.exec(`curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg && sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/ && sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'`);
+ shell.exec(`sudo apt-get install -y apt-transport-https`);
+ shell.exec(`sudo apt-get install -y code`);
  shell.echo(`"deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list`);
  shell.exec(`sudo apt-get update`); 
  shell.exec(`sudo apt-get install -y mongodb-org`);
- shell.exec(`curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg && sudo install -y -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/`)
- shell.exec(`sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'`);
- shell.exec(`sudo apt-get install -y code`);
  if (!shell.which('gitkraken')) 
 {
  shell.exec(`wget https://release.gitkraken.com/linux/gitkraken-amd64.deb`);
