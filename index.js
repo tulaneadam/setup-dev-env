b8d93976b98c5e008f37156a21c1c82865b6ea66
Adam Goldfine <tulaneadam21@gmail.com>
2019-10-17 22:25:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -70,8 +70,13 @@ shell.exec(`sudo apt-get install -y gnome-software gnome-packagekit`);
  shell.exec(`curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg && sudo install -y -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/`)
  shell.exec(`sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'`);
  shell.exec(`sudo apt-get install -y code`);
+ 
+ if (!shell.which('gitkraken')) 
+{
  shell.exec(`wget https://release.gitkraken.com/linux/gitkraken-amd64.deb`);
  shell.exec(`sudo apt install -y ./gitkraken-amd64.deb`);
+};
+ 
  
 }
 
