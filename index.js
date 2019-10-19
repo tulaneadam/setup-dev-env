0b7e70ed65cfa0ac7ca37faf4f424ba4ab4833d5
Adam Goldfine <tulaneadam21@gmail.com>
2019-10-19 03:00:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ shell.exec(`sudo apt-get install -y gnome-software gnome-packagekit`);
  //shell.exec(`sudo snap install code --classic`);
  shell.exec(`curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg  && sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ && sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' && sudo apt-get install apt-transport-https && sudo apt-get update && sudo apt-get install code`);
  shell.exec(`sudo apt-get install -y apt-transport-https`);
- shell.exec(`sudo apt-get install code`);
+ shell.exec(`sudo apt-get install -y code`);
  shell.exec(`sudo apt install -y mongodb`);
  if (!shell.which('gitkraken')) 
 {
