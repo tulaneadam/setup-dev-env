e16e5466d2a7fc62d417b801a8222399fca94beb
jk <kj>
2019-11-19 15:23:10 -0500

    updated jest test

--- index.test.js
+++ index.test.js
@@ -1,5 +1,15 @@
 const setupDevEnv = require('./index');
 
-it('should return the os info for the environment', () => {
-    expect(setupDevEnv.os).toBeDefined();
+it('should return the os info for the environment', async () => {
+    /*
+    try {
+        const ouput = await setupDevEnv();
+        expect(ouput.os).toBeDefined();
+    } catch (err) {
+        expect(err).toThrow(err);
+    }
+*/
+    setupDevEnv().then(ouput => {
+        expect(ouput.os).toBeDefined();
+    }).catch(err => expect(err).toThrow(err));
 });
